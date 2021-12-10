877d4197fa3751c15c9e92d016f758497a44dce3
Natan Boanafina <boanafina.natan@gmail.com>
2021-12-10 19:51:06 -0300

    Styles added

--- scripts/script.js
+++ scripts/script.js
@@ -11,10 +11,15 @@ inputSearch.addEventListener('input',(e)=>{
 const result = document.querySelector('.results');
 
 async function fetchingApi(word = "English"){
-  const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-  const data = await res.json();
+  try{
+    const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+      const data = await res.json();
+      render(data);
+  }
+  catch(e){
+      console.log(e);
+  }
 
-  render(data);
 }
 
 function render(data){
@@ -22,20 +27,20 @@ function render(data){
   data.filter(item => {
     console.log(item);
     output += `
-      <ul>
-      <li>Word: ${item.word}</li>
-      <li>Phonetic Written: ${item.phonetic}</li>
-      <li>Spelling: ${item.phonetics.map(item => `<span>${item.text}</span>
+      <ul class="container-sm font-monospace lh-lg">
+      <li><strong>Word</strong>: ${item.word}</li>
+      <li><strong>Phonetic Written</strong>: ${item.phonetic}</li>
+      <li><strong>Spelling</strong>: ${item.phonetics.map(item => `<span>${item.text}</span>
       <li>
       <audio controls>
       <source src="${item.audio}" type="audio/mp3">
       </audio>
       </li>
       `)}</li>
-      <li>Origin: ${item.origin}</li>
-      <li>Part of Speech: ${item.meanings.map(mean => mean.partOfSpeech)}</li>
-      <li>Definition: ${item.meanings.map(mean => mean.definitions.map(def => def.definition))}</li>
-      <li>Example: ${item.meanings.map(mean => mean.definitions.map(def => def.example))}</li>
+      <li><strong>Origin</strong>: ${item.origin}</li>
+      <li><strong>Part of Speech</strong>: ${item.meanings.map(mean => mean.partOfSpeech)}</li>
+      <li><strong>Definition</strong>: ${item.meanings.map(mean => mean.definitions.map(def => def.definition))}</li>
+      <li><strong>Example</strong>: ${item.meanings.map(mean => mean.definitions.map(def => def.example))}</li>
       </ul>
     `
   }) 
